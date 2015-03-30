09a668331c8a75a5e7b9e28cce2ecd2918304f10
Stu Robson <stu@alwaystwisted.com>
2015-03-30 07:34:21 +0100

    updates gulpfile.js so it works again

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,111 @@
 var gulp        = require('gulp'),
-sass        = require('gulp-sass'),
-rename      = require('gulp-rename'),
-cssmin      = require('gulp-minify-css'),
-concat      = require('gulp-concat'),
-uglify      = require('gulp-uglify'),
-cache       = require('gulp-cached'),
-prefix      = require('gulp-autoprefixer'),
-browserSync = require('browser-sync'),
-reload      = browserSync.reload,
-minifyHTML  = require('gulp-minify-html'),
-size        = require('gulp-size'),
-imagemin    = require('gulp-imagemin'),
-pngquant    = require('imagemin-pngquant'),
-plumber     = require('gulp-plumber'),
-notify      = require('gulp-notify'),
-connect     = require('gulp-connect-php'),
-httpProxy   = require('http-proxy');
+    sass        = require('gulp-sass'),
+    rename      = require('gulp-rename'),
+    cssmin      = require('gulp-minify-css'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify'),
+    cache       = require('gulp-cached'),
+    prefix      = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    reload      = browserSync.reload,
+    minifyHTML  = require('gulp-minify-html'),
+    size        = require('gulp-size'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    plumber     = require('gulp-plumber'),
+    notify      = require('gulp-notify'),
+    connect     = require('gulp-connect-php');
 
 
 gulp.task('scss', function() {
-  var onError = function(err) {
-    notify.onError({
-      title:    "Gulp",
-      subtitle: "Failure!",
-      message:  "Error: <%= error.message %>",
-      sound:    "Beep"
-    })(err);
-    this.emit('end');
+    var onError = function(err) {
+      notify.onError({
+          title:    "Gulp",
+          subtitle: "Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+      })(err);
+      this.emit('end');
   };
 
   return gulp.src('scss/main.scss')
-  .pipe(plumber({errorHandler: onError}))
-  .pipe(sass())
-  .pipe(size({ gzip: true, showFiles: true }))
-  .pipe(prefix())
-  .pipe(rename('main.css'))
-  .pipe(gulp.dest('dist/css'))
-  .pipe(reload({stream:true}))
-  .pipe(cssmin())
-  .pipe(size({ gzip: true, showFiles: true }))
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('css'))
+    .pipe(plumber({errorHandler: onError}))
+    .pipe(sass())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(prefix())
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(reload({stream:true}))
+    .pipe(cssmin())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('php-serve', ['scss'], function () {
-  connect.server({
-    port: 9001,
-    base: 'dist',
-    open: false
-  });
-
-  var proxy = httpProxy.createProxyServer({});
-
-  browserSync({
-    notify: true,
-    port  : 9000,
-    server: {
-      baseDir   : ['.tmp', 'dist'],
-      routes    : {
-        '/bower_components': 'bower_components'
-      },
-      middleware: function (req, res, next) {
-        var url = req.url;
-
-        if (!url.match(/^\/(styles|fonts|bower_components)\//)) {
-          proxy.web(req, res, { target: 'http://127.0.0.1:9001' });
-        } else {
-          next();
-        }
-      }
-    }
-  });
-
-  // watch for changes
-  gulp.watch([
-    './*.html',
-    './*.php',
-    './scss/**/*.scss',
-    './scripts/**/*.js',
-    './images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
-
-  gulp.watch('./styles/**/*.scss', ['styles']);
-  gulp.watch('bower.json', ['wiredep']);
-});
+    gulp.task('connect-sync', function() {
+      connect.server({
+        baseDir: './dist'
+      }, function(){
+        browserSync({
+          proxy: 'localhost:8000'
+        });
+      });
+    })
 
 gulp.task('deploy', function () {
-  return gulp.src('dist/**/*')
-  .pipe(deploy());
+    return gulp.src('dist/**/*')
+        .pipe(deploy());
 });
 
 gulp.task('copy', function() {
   gulp.src(['markup/**/*'])
-  .pipe(gulp.dest('dist/markup'))
+    .pipe(gulp.dest('dist/markup'))
+  gulp.src(['scss/**/*'])
+    .pipe(gulp.dest('dist/'))
   gulp.src(['./index.php', './functions.php'])
-  .pipe(gulp.dest('dist/'))
-  gulp.src(['./css/**/*.css'])
-  .pipe(gulp.dest('dist/css/'))
-  gulp.src(['./js/**/*.js'])
-  .pipe(gulp.dest('dist/js/'))
+    .pipe(gulp.dest('dist/'))
+  gulp.src(['./css/sg-style.css'])
+    .pipe(gulp.dest('dist/css/'))
 });
 
 gulp.task('js', function() {
   gulp.src('js/*.js')
-  .pipe(uglify())
-  .pipe(size({ gzip: true, showFiles: true }))
-  .pipe(concat('scripts.js'))
-  .pipe(gulp.dest('dist/js'))
-  .pipe(reload({stream:true}));
+    .pipe(uglify())
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('dist/js'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('minify-html', function() {
+    var opts = {
+      comments:true,
+      spare:true
+    };
+
+  gulp.src('./*.php')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('dist/'))
+    .pipe(reload({stream:true}));
 });
 
 gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', ['scss']);
   gulp.watch('js/*.js', ['js']);
+  gulp.watch('./*.php', ['minify-html']);
   gulp.watch('img/*', ['imgmin']);
 });
 
 gulp.task('imgmin', function () {
-  return gulp.src('img/*')
-  .pipe(imagemin({
-    progressive: true,
-    svgoPlugins: [{removeViewBox: false}],
-    use: [pngquant()]
-  }))
-  .pipe(gulp.dest('dist/img'));
+    return gulp.src('img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('default', ['js', 'imgmin', 'scss', 'watch', 'php-serve']);
-gulp.task('build', ['js', 'imgmin', 'scss', 'copy']);
+gulp.task('default', ['js', 'imgmin', 'copy', 'scss', 'watch', 'connect-sync']);
+
+gulp.task('default', ['copy', 'scss', 'connect-sync']);

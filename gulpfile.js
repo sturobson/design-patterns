7fcdcaef76fb3fbc0f40dde204502740a59190ad
Stu Robson <stu@alwaystwisted.com>
2015-03-30 07:52:18 +0100

    updates to have 'serve' and a 'dist'

--- gulpfile.js
+++ gulpfile.js
@@ -1,98 +1,142 @@
 var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    rename      = require('gulp-rename'),
-    cssmin      = require('gulp-minify-css'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    cache       = require('gulp-cached'),
-    prefix      = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync'),
-    reload      = browserSync.reload,
-    size        = require('gulp-size'),
-    imagemin    = require('gulp-imagemin'),
-    pngquant    = require('imagemin-pngquant'),
-    plumber     = require('gulp-plumber'),
-    notify      = require('gulp-notify'),
-    connect     = require('gulp-connect-php');
+sass            = require('gulp-sass'),
+rename          = require('gulp-rename'),
+cssmin          = require('gulp-minify-css'),
+concat          = require('gulp-concat'),
+uglify          = require('gulp-uglify'),
+cache           = require('gulp-cached'),
+prefix          = require('gulp-autoprefixer'),
+browserSync     = require('browser-sync'),
+reload          = browserSync.reload,
+size            = require('gulp-size'),
+imagemin        = require('gulp-imagemin'),
+pngquant        = require('imagemin-pngquant'),
+plumber         = require('gulp-plumber'),
+notify          = require('gulp-notify'),
+connect         = require('gulp-connect-php'),
+httpProxy       = require('http-proxy');
 
 
 gulp.task('scss', function() {
-    var onError = function(err) {
-      notify.onError({
-          title:    "Gulp",
-          subtitle: "Failure!",
-          message:  "Error: <%= error.message %>",
-          sound:    "Beep"
-      })(err);
-      this.emit('end');
+  var onError = function(err) {
+    notify.onError({
+      title:    "Gulp",
+      subtitle: "Failure!",
+      message:  "Error: <%= error.message %>",
+      sound:    "Beep"
+    })(err);
+    this.emit('end');
   };
 
   return gulp.src('scss/main.scss')
-    .pipe(plumber({errorHandler: onError}))
-    .pipe(sass())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(prefix())
-    .pipe(rename('main.css'))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(reload({stream:true}))
-    .pipe(cssmin())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/css'))
+  .pipe(plumber({errorHandler: onError}))
+  .pipe(sass())
+  .pipe(size({ gzip: true, showFiles: true }))
+  .pipe(prefix())
+  .pipe(rename('main.css'))
+  .pipe(gulp.dest('css'))
+  .pipe(reload({stream:true}))
+});
+gulp.task('scss:dist', function() {
+  var onError = function(err) {
+    notify.onError({
+      title:    "Gulp",
+      subtitle: "Failure!",
+      message:  "Error: <%= error.message %>",
+      sound:    "Beep"
+    })(err);
+    this.emit('end');
+  };
+
+  return gulp.src('scss/main.scss')
+  .pipe(plumber({errorHandler: onError}))
+  .pipe(sass())
+  .pipe(prefix())
+  .pipe(cssmin())
+  .pipe(size({ gzip: true, showFiles: true }))
+  .pipe(gulp.dest('dist/css'))
 });
 
-    gulp.task('connect-sync', function() {
-      connect.server({
-        baseDir: './dist'
-      }, function(){
-        browserSync({
-          proxy: 'localhost:8000'
-        });
-      });
-    })
+gulp.task('connect-sync', function() {
+  connect.server({
+    baseDir: './dist'
+  }, function(){
+    browserSync({
+      proxy: 'localhost:8000'
+    });
+  });
+})
+
+gulp.task('php-serve', ['scss'], function () {
+  connect.server({
+    port: 3001,
+    base: 'dist',
+    open: false
+  });
+
+  var proxy = httpProxy.createProxyServer({});
+
+  browserSync({
+    notify: true,
+    port  : 3000,
+    server: {
+      baseDir   : ['.tmp', 'dist']
+    }
+  });
+
+  // watch for changes
+  gulp.watch([
+    './*.html',
+    './*.php',
+    './scss/**/*.scss',
+    './scripts/**/*.js',
+    './images/**/*',
+    '.tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('./styles/**/*.scss', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
 
 gulp.task('deploy', function () {
-    return gulp.src('dist/**/*')
-        .pipe(deploy());
+  return gulp.src('dist/**/*')
+  .pipe(deploy());
 });
 
-gulp.task('copy', function() {
+gulp.task('copy:dist', function() {
   gulp.src(['markup/**/*'])
-    .pipe(gulp.dest('dist/markup'))
-  gulp.src(['scss/**/*'])
-    .pipe(gulp.dest('dist/'))
+  .pipe(gulp.dest('dist/markup'))
   gulp.src(['./index.php', './functions.php'])
-    .pipe(gulp.dest('dist/'))
+  .pipe(gulp.dest('dist/'))
   gulp.src(['./css/sg-style.css'])
-    .pipe(gulp.dest('dist/css/'))
+  .pipe(gulp.dest('dist/css/'))
 });
 
 gulp.task('js', function() {
   gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(size({ gzip: true, showFiles: true }))
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(reload({stream:true}));
+  .pipe(uglify())
+  .pipe(size({ gzip: true, showFiles: true }))
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(reload({stream:true}));
 });
 
 gulp.task('watch', function() {
   gulp.watch('scss/**/*.scss', ['scss']);
   gulp.watch('js/*.js', ['js']);
-  gulp.watch('./*.php', ['minify-html']);
   gulp.watch('img/*', ['imgmin']);
 });
 
 gulp.task('imgmin', function () {
-    return gulp.src('img/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('dist/img'));
+  return gulp.src('img/*')
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngquant()]
+  }))
+  .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('default', ['js', 'imgmin', 'copy', 'scss', 'watch', 'connect-sync']);
-
-gulp.task('serve', ['copy', 'scss', 'connect-sync']);
+gulp.task('default', ['js', 'imgmin', 'scss', 'watch', 'connect-sync']);
+gulp.task('build', ['js', 'imgmin', 'scss:dist', 'copy:dist', 'scss:dist']);
+gulp.task('serve', ['scss', 'connect-sync']);
